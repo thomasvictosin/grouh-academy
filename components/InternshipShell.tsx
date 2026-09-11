204bcb42276a39b3f936107c9aa310f063189564
thomasvictosin <thomasvictosin@gmail.com>
2026-09-11 13:09:03 +0100

    Complete mobile responsiveness

--- components/InternshipShell.tsx
+++ components/InternshipShell.tsx
@@ -26,7 +26,8 @@ export default function InternshipShell({ children }: { children: React.ReactNod
       return
     }
 
-    setAccessChecked(true)
+    const accessCheck = window.setTimeout(() => setAccessChecked(true), 0)
+    return () => window.clearTimeout(accessCheck)
   }, [isDevelopmentPreview, isEnrollmentRoute, isPaymentRoute, router])
 
   if (!accessChecked) {
